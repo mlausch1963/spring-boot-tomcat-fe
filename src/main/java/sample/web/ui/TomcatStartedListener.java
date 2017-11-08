13055ad50f98b1428ad97521546c2b969d5ca49d
Michael Lausch <mla@lausch.at>
2017-11-08 21:15:05 +0100

    Code Cleanup and be more explicit.

--- src/main/java/sample/web/ui/TomcatStartedListener.java
+++ src/main/java/sample/web/ui/TomcatStartedListener.java
@@ -3,18 +3,18 @@ package sample.web.ui;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedWebappClassLoader;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import io.micrometer.core.instrument.MeterRegistry;
 
 @Component
-@ConditionalOnClass(TomcatEmbeddedWebappClassLoader.class)
+@ConditionalOnClass(Tomcat.class)
 public class TomcatStartedListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
 
 	@Autowired
@@ -27,14 +27,11 @@ public class TomcatStartedListener implements ApplicationListener<EmbeddedServle
     public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
 
 		TomcatEmbeddedServletContainer tomcat = (TomcatEmbeddedServletContainer) event.getSource();
-		Executor e = tomcat.getTomcat().getConnector().getProtocolHandler().getExecutor();
-		ThreadPoolExecutor tpe = (ThreadPoolExecutor)e;
-		
-		if (tomcat.getTomcat().getService().getDomain().equals("Tomcat")){
-            //metrics = new TomcatThreadPoolMetrics(tpe);
+        ThreadPoolExecutor tpe = (ThreadPoolExecutor) tomcat.getTomcat()
+                .getConnector().getProtocolHandler().getExecutor();
+
+        if (tomcat.getTomcat().getService().getDomain().equals("Tomcat")){
             metrics.setExecutor(tpe);
-            // This is done by spring boot autoconf, before we even arrive here.
-			//metrics.bindTo(registry);
 		}
 	}
 }
